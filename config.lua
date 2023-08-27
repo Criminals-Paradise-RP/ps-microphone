a0de2cae0875d67b70a75b42ca1f253d7f8f6dbc
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-08-26 19:41:17 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,16 +4,16 @@ Config.Showzone = false
 
 Config.MicrophoneZones = {
     [1] = {
-        name = "vinewood_bowl", 
-        coords = vector3(683.37, 569.31, 130.46),
+        name = "mbs_concert", 
+        coords = vector3(-322.35, -1966.4, 22.94),
         length = 3.4,
         width = 3.6,
-        spawnProp = true, -- if set to true, it will let you spawn the prop at location
+        spawnProp = false, -- if set to true, it will let you spawn the prop at location
         data = {
             debugPoly = Config.Showzone,
-            heading = 340,
-            minZ = 127.86,
-            maxZ = 131.86,
+            heading = 316.03,
+            minZ = 19.86,
+            maxZ = 25.86,
             data = {
                 range = 50.0 -- range for the voice
             }
